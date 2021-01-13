34a25fb9adc2ce7c3eb10e6f16788af9c6b2bc3e
donovanrichardson <donovanspqa@gmail.com>
2021-01-12 20:54:29 -0500

    added function for twelve-hour clock

--- src/views/DateScreen.vue
+++ src/views/DateScreen.vue
@@ -20,8 +20,11 @@ export default {
     name: 'DateScreen',
     created: async function(){
         const dquery = this.$route.query
+        const url = `http://localhost:3001/timezone?agency=${dquery.agency}`
+        console.log('why no work', url);
         const {data} = await axios.get(`http://localhost:3001/timezone?agency=${dquery.agency}`)
         this.calendarDate = DateTime.fromObject({zone:data[0].agecny_timezone}).toFormat("yyyy-MM-dd")
+        console.log(DateTime, this.calendarDate);
     },
     data: function(){
         return{
--- src/views/Timetable.vue
+++ src/views/Timetable.vue
@@ -1,5 +1,5 @@
 <template>
-<div >
+<div>
     <p v-for="time in times" :key="time">
         {{time}}
     </p>
@@ -7,33 +7,50 @@
 </template>
 
 <script>
-    import axios from 'axios'
-    import {DateTime} from 'luxon'
-export default {
-// import FlexDisplay from '../components/FlexDisplay'
-name:"Timetable",
-created: async function(){
-    const dquery = this.$route.query
-    // { "agency": "boston", "date": "20201206", "route": "1", "origin": "64", "dest": "110" } 
-    const url = `http://localhost:3001/timetable?agency=${dquery.agency}&date=${dquery.date}&route=${dquery.route}&origin=${dquery.origin}&dest=${dquery.dest}`
-    const {data} = await axios.get(url)
-    this.timetable = data
-},
-data(){
-    return{
-        timetable:null
-    }
-},
-computed:{
-    times(){
-        if(this.timetable){
-            return this.timetable.map(t=>{
-                return t.departure_time
-                // return DateTime.fromFormat(t.departure_time, "hh:mm:ss", {zone:"UTC"}).toString()
-            })
-        }
-    }
+import axios from 'axios'
+import {
+    DateTime
+} from 'luxon'
+
+function twelveHour(theHour){
+    return DateTime.utc().startOf('day').plus({hour:theHour}).toFormat('ha')
 }
+
+export default {
+    // import FlexDisplay from '../components/FlexDisplay'
+    name: "Timetable",
+    created: async function () {
+        const dquery = this.$route.query
+        // { "agency": "boston", "date": "20201206", "route": "1", "origin": "64", "dest": "110" } 
+        const url = `http://localhost:3001/timetable?agency=${dquery.agency}&date=${dquery.date}&route=${dquery.route}&origin=${dquery.origin}&dest=${dquery.dest}`
+        const {
+            data
+        } = await axios.get(url)
+        this.timetable = data
+        this.times = this.timetable.map(t => {
+            return t.departure_time.split(':').slice(0, 2)
+            // return DateTime.fromFormat(t.departure_time, "hh:mm:ss", {zone:"UTC"}).toString()
+        })
+    },
+    data() {
+        return {
+            timetable: null,
+            times: [],
+            min:null,
+            max:null,
+            hr12:false,
+        }
+    },
+    computed: {
+        // times(){
+        //     if(this.timetable){
+        //         return this.timetable.map(t=>{
+        //             return t.departure_time.split(':').slice(0,2)
+        //             // return DateTime.fromFormat(t.departure_time, "hh:mm:ss", {zone:"UTC"}).toString()
+        //         })
+        //     }
+        // }
+    }
 }
 </script>
 
