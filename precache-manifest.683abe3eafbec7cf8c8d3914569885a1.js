5cc3939442db8c1cfc2843e9659d0e3be0597d6c
donovanrichardson <donovanspqa@gmail.com>
2021-01-21 15:22:06 -0500

    deploy

---
+++ precache-manifest.683abe3eafbec7cf8c8d3914569885a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1bfa40be4aaa75eea5",
+    "url": "/deparch/css/app.0482158c.css"
+  },
+  {
+    "revision": "ae4cc08d6622c288e463",
+    "url": "/deparch/css/chunk-vendors.418e163a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/deparch/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "876fd14948877e5755eeef972f7e9f03",
+    "url": "/deparch/index.html"
+  },
+  {
+    "revision": "da1bfa40be4aaa75eea5",
+    "url": "/deparch/js/app.2e714c9d.js"
+  },
+  {
+    "revision": "ae4cc08d6622c288e463",
+    "url": "/deparch/js/chunk-vendors.895fbf21.js"
+  },
+  {
+    "revision": "992b6fce3435e9de82f00dec59dfa215",
+    "url": "/deparch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deparch/robots.txt"
+  }
+]);
